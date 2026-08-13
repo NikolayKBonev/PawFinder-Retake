c1ca4b29bacc2a885dde1cd8b4a123e93a1e3d31
NikolayKBonev <snayperista87@gmail.com>
2026-08-13 17:02:30 +0300

    init commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,7 +3,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableSchedulin;
 
 @SpringBootApplication
 @EnableFeignClients
